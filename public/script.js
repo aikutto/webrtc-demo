1320a950da6107badd89493d8cd1da5481075efd
aykut <aykut@erbakan.edu.tr>
2022-11-01 12:13:14 +0300

    remove fetch api

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const { createApp } = Vue;
+const {createApp} = Vue;
 createApp({
     data() {
         return {
@@ -8,31 +8,51 @@ createApp({
                 username: null,
             },
             setUsernameFormErrorMessage: 'Please provide an username.',
-
+            pc: null,
+            usernameToCall: null,
+            callUsernameForm: {
+                username: null,
+            },
+            callUsernameFormErrorMessage: '',
         };
     },
     methods: {
         setUsername() {
-            fetch('/set-username', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    username: this.setUsernameForm.username,
-                    socketId: this.socket.id,
-                })
-            }).then((res) => res.json())
-                .then((data) => {
-                    if(data.success){
-                        this.username = data.username;
-                    } else {
-                        this.setUsernameFormErrorMessage = data.message;
-                    }
-                });
+            this.socket.emit('set-username', this.setUsernameForm.username);
+        },
+        callUsername() {
+            if (this.setUsernameForm.username === this.callUsernameForm.username) {
+                this.callUsernameFormErrorMessage = 'You can not call yourself.';
+            } else {
+
+            }
         }
     },
     mounted() {
-
+        this.socket.on('set-username-response', (data) => {
+                if (data.success) {
+                    this.username = data.username;
+                    this.pc = new RTCPeerConnection({
+                        iceServers: [
+                            {urls: 'stun:stun.l.google.com:19302'},
+                            {urls: 'stun:stun1.l.google.com:19302'},
+                            {urls: 'stun:stun2.l.google.com:19302'},
+                            {urls: 'stun:stun3.l.google.com:19302'},
+                            {urls: 'stun:stun4.l.google.com:19302'},
+                            {urls: 'stun:stunserver.org'},]
+                    });
+                    navigator.mediaDevices.getUserMedia({
+                        // video: true,
+                        audio: true,
+                    }).then((stream) => {
+                        stream.getTracks().forEach((track) => {
+                            this.pc.addTrack(track);
+                        });
+                    });
+                } else {
+                    this.setUsernameFormErrorMessage = data.message;
+                }
+            }
+        );
     }
 }).mount('#app');
--- server.js
+++ server.js
@@ -4,42 +4,38 @@ const parser = require('body-parser');
 
 app.use(express.static('public'));
 app.use(parser.json());
-app.use(parser.urlencoded({ extended: true }));
+app.use(parser.urlencoded({extended: true}));
 
 let users = {};
-
-app.post('/set-username', (req, res) => {
-    console.log(users);
-    if (!users[req.body.username]) {
-        users[req.body.username] = {
-            socketId: req.body.socketId
-        };
-        res.json({
-            success: true,
-            username: req.body.username
-        });
-    } else {
-        res.json({
-            success: false,
-            message: `Username ${req.body.username} is already taken.`
-        });
-    }
-});
 
 const server = app.listen(3000, () => {
     console.log('listening on *:3000');
 });
-const { Server } = require('socket.io');
+const {Server} = require('socket.io');
 const io = new Server(server);
 
 io.on('connection', (socket) => {
-    console.log(socket.id + ' connnected...');
     socket.on('disconnect', () => {
         Object.keys(users).forEach((username) => {
-            if (users[username].socketId == socket.id) {
+            if (users[username].socketId === socket.id) {
                 delete users[username];
-                console.log(users);
             }
         });
+    });
+    socket.on('set-username', (username) => {
+        if (!users[username]) {
+            users[username] = {
+                socketId: socket.id
+            };
+            socket.emit('set-username-response', {
+                success: true,
+                username: username
+            });
+        } else {
+            socket.emit('set-username-response', {
+                success: false,
+                message: `Username ${username} is already taken.`
+            });
+        }
     });
 });
